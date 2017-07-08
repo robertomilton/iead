2b00058d2c1711e6845a97c403d22a1c4f4f7ecf
rg3915 <rg3915@yahoo.com.br>
2017-07-07 22:24:24 -0300

    Retornando as pessoas em person_list

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -3,5 +3,5 @@ from myproject.core import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^persons/$', views.persons, name='persons'),
+    url(r'^persons/$', views.person_list, name='persons'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Person
 
 
 def home(request):
     return render(request, 'core/index.html')
 
 
-def persons(request):
-    return render(request, 'core/persons.html')
+def person_list(request):
+    persons = Person.objects.all()
+    ctx = {'persons': persons}
+    return render(request, 'core/person_list.html', ctx)
