abd125505b9eb9258aef286f5b888ab45e54030b
rg3915 <rg3915@yahoo.com.br>
2017-07-07 22:31:12 -0300

    Campo de busca no template

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -8,5 +8,8 @@ def home(request):
 
 def person_list(request):
     persons = Person.objects.all()
+    get_search = request.GET.get('search_box')
+    if get_search is not None:
+        persons = persons.filter(nome__icontains=get_search)
     ctx = {'persons': persons}
     return render(request, 'core/person_list.html', ctx)
